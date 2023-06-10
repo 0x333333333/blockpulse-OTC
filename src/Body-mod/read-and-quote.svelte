7427a2f55c8d3036ddeabb603b294c023d70fcbb
0x333333333 <69386697+0x333333333@users.noreply.github.com>
2023-06-10 10:57:24 +0800

    2 files modified, 1 added

---
+++ src/Body-mod/read-and-quote.svelte
@@ -0,0 +1,112 @@
+<script>
+  let act =  ['BUY', 'SELL'];
+  let fiat = ['HKD', 'USD'];
+  let crypto = ['BTC', 'ETH', 'USDT'];
+  let selectedOption0 = act[0];  
+  let selectedOption1 = fiat[0];
+  let selectedOption2 = crypto[0];
+  let inputValue = '';
+  let outputValue = '';
+</script>
+
+<style>
+
+  .big-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center; /* 添加居中对齐 */
+    padding: 0.3em;
+    background-color: #006590;
+    border-radius: 14px;
+  }
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    width: 100%;
+    gap: 0.1em;
+    padding: 0.3em;
+    border-radius: 14px;
+  }
+
+  .select {
+    flex: 1;
+    max-width: 300px;
+    height: 1.6em;
+    padding: 0.1em;
+  }
+
+  .select select {
+    width: 100%;
+    height: 100%;
+    padding: 4px;
+    background-color: #006590;
+    border-radius: 4px;
+    border: 1px solid #444444;
+    color: white;
+  }
+
+  .input {
+    flex: 1;
+    width: 39px;
+    height: 1.6em;
+    background-color: #006590;
+    border-radius: 4px;
+    border: 1px solid #444444;
+    color: white;
+    text-align: center;
+  }
+
+  .grey {
+    color: #444444;
+  }
+  .white {
+    color: #ffffff;
+    font-size: 16px;
+  }
+</style>
+
+
+<div class="big-container">
+
+
+<div class="container">
+
+  <div class="select">
+    <select bind:value={selectedOption0}>
+      {#each act as option}
+        <option value={option}>{option}</option>
+      {/each}
+    </select>
+  </div>
+
+    <input class="input" type="number" bind:value={inputValue} />
+
+  <div class="select">
+    <select bind:value={selectedOption1}>
+      {#each crypto as option}
+        <option value={option}>{option}</option>
+      {/each}
+    </select>
+  </div></div>
+
+
+<div class="container">
+  <p class="white">WITH</p>
+
+  <div class="select">
+    <select bind:value={selectedOption2}>
+      {#each fiat as option}
+        <option value={option}>{option}</option>
+      {/each}
+    </select>
+  </div>
+
+  <p class="white">BY NOW</p>
+
+</div>
+
+<br><button>Quote</button>
+
+</div>
